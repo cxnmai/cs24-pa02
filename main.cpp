dac6b30726273a4c6ee65a323088e8ac88112436
Chinmay Khaladkar <chinmaykhaladkar1@gmail.com>
2026-05-22 18:49:18 -0700

    commented answers to 3

--- main.cpp
+++ main.cpp
@@ -91,7 +91,31 @@ int main(int argc, char** argv){
 // Since at most k movies match, they have to be sorted, which is O(k*log(k))
 // Multiply that by l since that's the maximum amount of letters that need to be compared
 // yielding O(k*log(k) * l)
-// Adding those and multiplying by m movies gives O(m(n*l + k*log(k)*l))
+// Adding those and multiplying by m prefixes gives O(m(n*l + k*log(k)*l))
+// 
+// My laptop's runtimes:
+// > sh time_prefix_large.sh
+// input_20_random.csv: 9 ms
+// input_100_random.csv: 23 ms
+// input_1000_random.csv: 191 ms
+// input_76920_random.csv: 10269 ms
+// 
+// Spcae complexity:
+// Storing the movies in pairs becomes O(n*l) bc n movies * l length to be checked
+// Storing the matching movies for m prefixes with k matches per prefix up to length l becomes O(m*k*l)
+// So in total the space complexity is O(n*l + m*k*l)
+// 
+// 3c answer:
+// My approach to this project prioritized lowering the time complexity by storing the entire sorted
+// list of movies once to prevent repeated work. The targeted complexities were my answers from above,
+// because my goal was to store all the movies then also store up to k matching movies per prefix.
+// 
+// I was able to keep space complexity reasonably low. Storing the dataset once is necessary -
+// past that I store the prefix match results to enable faster retrieval for printing afterwards,
+// a tradeoff that in my opinion is acceptable given the problem constraints.
+// That could be large in the worst cases, but usually k is much smaller than n so it's manageable.
+
+
 bool parseLine(string &line, string &movieName, double &movieRating) {
     int commaIndex = line.find_last_of(",");
     movieName = line.substr(0, commaIndex);
